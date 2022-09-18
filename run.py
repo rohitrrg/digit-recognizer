bf85ed525b35873bfb1f012daaf2c8c8809fa550
rohitrrg <rohitgadhwar05@gmail.com>
2022-09-18 13:58:19 +0530

    new

---
+++ run.py
@@ -0,0 +1,40 @@
+from flask import Flask, app, render_template, request, redirect, flash, url_for, jsonify
+import cv2
+import numpy as np
+import base64
+from keras.models import load_model
+
+init_Base64 = 21
+
+ann_model = load_model('models/my_keras_model.h5')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.route('/', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         draw = request.form['url']
+#         draw = draw[init_Base64:]
+#         draw_decoded = base64.b64decode(draw)
+#         image = np.asarray(bytearray(draw_decoded))
+#         image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+#         resized = cv2.resize(image, (28,28), interpolation = cv2.INTER_AREA)
+#         name = '/static/genda.jpg'
+#         cv2.imwrite(name ,image)
+        
+#         # Neural Networks
+#         img = (np.expand_dims(resized, 0))
+#         ann_result = ann_model.predict(img/255.0)
+    
+
+
+#     return render_template('index.html',
+#                            ann_result = np.argmax(ann_result, axis=1)[0], 
+#                            ann_probs=ann_result[0].round(2), )
+
+if __name__=="__main__":
+    app.run(port="8000", debug=True)
